1f190fec5f37636da7d15ed95677efdd936b6e18
Gabriel Guivisdalsky <103234446+Franchuu21@users.noreply.github.com>
2023-11-01 09:55:11 -0300

    ..

--- src/ClipBoard.jsx
+++ src/ClipBoard.jsx
@@ -1,40 +1,27 @@
-import React, {useState} from 'react';
-import {
-  SafeAreaView,
-  View,
-  Text,
-  TouchableOpacity,
-  Clipboard,
-  StyleSheet,
-} from 'react-native';
+import * as React from 'react';
+import { View, Text, Button, StyleSheet } from 'react-native';
+import * as Clipboard from 'expo-clipboard';
 
-const App = () => {
-  const [copiedText, setCopiedText] = useState('');
+export default function App() {
+  const [copiedText, setCopiedText] = React.useState('');
 
-  const copyToClipboard = () => {
-    Clipboard.setString('hello world');
+  const copyToClipboard = async () => {
+    await Clipboard.setStringAsync('hello world');
   };
 
   const fetchCopiedText = async () => {
-    const text = await Clipboard.getString();
+    const text = await Clipboard.getStringAsync();
     setCopiedText(text);
   };
 
   return (
-    <SafeAreaView style={{flex: 1}}>
-      <View style={styles.container}>
-        <TouchableOpacity onPress={() => copyToClipboard()}>
-          <Text>Click here to copy to Clipboard</Text>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={() => fetchCopiedText()}>
-          <Text>View copied text</Text>
-        </TouchableOpacity>
-
-        <Text style={styles.copiedText}>{copiedText}</Text>
-      </View>
-    </SafeAreaView>
+    <View style={styles.container}>
+      <Button title="Click here to copy to Clipboard" onPress={copyToClipboard} />
+      <Button title="View copied text" onPress={fetchCopiedText} />
+      <Text style={styles.copiedText}>{copiedText}</Text>
+    </View>
   );
-};
+}
 
 const styles = StyleSheet.create({
   container: {
@@ -47,5 +34,3 @@ const styles = StyleSheet.create({
     color: 'red',
   },
 });
-
-export default App;
